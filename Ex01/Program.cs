3da4060731ac9f6074b17c68e3688b095ece098f
Staicu Adrian <adrianstacu888@gmail.com>
2020-02-10 01:37:39 +0200

    Add project files.

---
+++ Ex01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Scrieti un program care stocheaza intr-un vector 3 variabile de tip double declarate de voi si le afiseaza parcurgand vectorul.
+
+            double[] arr;
+
+            arr = new double[3];
+
+            arr[0] = 1.5;
+            arr[1] = 2.8;
+            arr[2] = 3.7;
+
+            for (int i = 0; i < arr.Length; i++)
+                Console.WriteLine("Element at index " + i + " : " + arr[i]);
+
+        }
+    }
+}
